81301f38e7a0ba327322a3f097779184f40f2999
James Burns <jrburns96@aol.com>
2021-10-21 15:42:24 -0400

    Couple little things

---
+++ script2.js
@@ -0,0 +1,28 @@
+let featuredTitle = document.querySelector("h1");
+featuredTitle.id = "featured-title";
+
+let featuredTitle2 = document.querySelector("h2");
+featuredTitle2.id = "featured-title2";
+
+let featuredTitle3 = document.querySelector("h3");
+featuredTitle3.id = "featured-title3";
+
+setTimeout(function() {
+    var elem4 = document.getElementById("featured-title3");
+    elem4.style.color = "red";
+}, 1000);
+
+setTimeout(function() {
+    var elem = document.getElementById("featured-title");
+    elem.parentNode.removeChild(elem);
+}, 3000);
+
+setTimeout(function() {
+    var elem2 = document.getElementById("featured-title2");
+    elem2.parentNode.removeChild(elem2);
+}, 3000);
+
+setTimeout(function() {
+    var elem3 = document.getElementById("featured-title3");
+    elem3.parentNode.removeChild(elem3);
+}, 3000);
